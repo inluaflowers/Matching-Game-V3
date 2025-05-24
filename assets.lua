f114510c2809ed05f6ce51b1dc0f73985a4db041
inluaflowers <inluawisher@gmail.com>
2025-05-24 09:52:58 -0700

    assets.lua complete

--- assets.lua
+++ assets.lua
@@ -1,23 +1,57 @@
-local Assets = {
-}
+local T = require('tools')
+local Assets = {}
 
+-- Built in string:Match % maies . literal, $ looks at end of the string
+local function check_for_PNG_ext(file_name)
+  return file_name:match('%.png$')
+end
 
-local function remove_PNG_extension(filename)
-  return filename:gsub("%.png$", "")
+local function remove_PNG_ext(file_name)
+  return file_name:gsub("%.png$", "")
 end
 
 local function load_assets()
+  -- Root Directory relative to main.lua
   local sprite_root = "Sprites"
   -- If there isn't a directory, print an error message
   if not love.filesystem.getInfo(sprite_root) then
     print("Sprite directory not found: " .. sprite_root)
     return
   end
+  -- All sprite category subdirectories
   local categories = love.filesystem.getDirectoryItems(sprite_root)
   for _, category in ipairs(categories) do
     local category_path = sprite_root .. "/" .. category
     local category_info = love.filesystem.getInfo(category_path)
-    print(category_info)
+    -- Checks to make sure category info is not nil and that the type is a directory
+    if category_info and category_info.type == 'directory'then
+      Assets[category] = {}
+      -- All files in current category directory
+      local files = love.filesystem.getDirectoryItems(category_path)
+      -- Used to print number of files loaded into assets by category
+      local file_count = 0
+      -- Will run the next loop so long as category directory isn't empty
+      if T.has_items(files) then
+        for _, file in ipairs(files) do
+          file_count = file_count + 1
+          if check_for_PNG_ext(file) then
+            local file_path = category_path .. '/' .. file
+            local sprite = love.graphics.newImage(file_path)
+            local asset_name = remove_PNG_ext(file)
+
+            Assets[category][asset_name] ={
+              name = asset_name,
+              sprite = sprite,
+              width = sprite:getWidth(),
+              height = sprite:getHeight()
+            }
+          end
+        end
+      print(file_count .. ' assests loaded from ' .. category)
+      else
+        print("No Files in", category_path)
+      end
+    end
   end
 end
 
--- tools.lua
+++ tools.lua
@@ -1,13 +1,24 @@
 local Tools = {}
-Tools.__index =  Tools
 
-function Tools:is_table(obj)
+function Tools.table_length(input_table)
+  local count = 0
+  for _ in pairs(input_table) do
+    count = count + 1
+  end
+  return count
+end
+
+function Tools.has_items(input_table)
+  return Tools.table_length(input_table) > 0
+end
+
+function Tools.is_table(obj)
   return type(obj) == "table"
 end
 
-function Tools:print_table(input_table, table_name)
-  if self:is_table(input_table) then
-    print(table_name or "No Name Given")
+function Tools.print_table(input_table, table_name)
+  if Tools.is_table(input_table) then
+    print('Table Name:', (table_name or "No Name Given"))
     for k, v in pairs(input_table) do
       print(k, v)
     end
